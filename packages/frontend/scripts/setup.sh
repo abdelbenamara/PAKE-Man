b9e8fc3dc98abe1b8de0c067b5c676b614eb70ef
Abdel Benamara <abdelb.contact@gmail.com>
2025-08-18 19:39:51 +0200

    feat(HTTPS): nginx proxy for backend, frontend and kibana

--- packages/frontend/scripts/setup.sh
+++ packages/frontend/scripts/setup.sh
@@ -9,7 +9,6 @@ generate_key() {
 main() {
   {
     echo "PAKE_MAN_COOKIE_SECRET=\"$(generate_key)\""
-    echo "PAKE_MAN_DOMAIN_NAME=\"pake-man.fun\""
     echo "PAKE_MAN_SERVER_CLOSE_GRACE_DELAY=\"1000\""
     echo "PAKE_MAN_SERVER_HOST=\"127.0.0.1\""
     echo "PAKE_MAN_SERVER_PORT=\"3000\""
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,11 +15,21 @@ main() {
     echo "PAKE_MAN_CSRF_PROTECTION_HMAC_KEY=\"$(generate_key)\""
     echo "PAKE_MAN_DOMAIN_NAME=\"pake-man.fun\""
 	} > .env
-  for pkg in $parentdir/packages/*/; do \
+  
+	for pkg in $parentdir/packages/*/; do \
 		if [ -d "$pkg" ]; then \
 			make -C "$pkg" setup; \
 		fi; \
 	done
+
+	python3 -m venv venv \
+        && source venv/bin/activate \
+        && pip install --upgrade pip \
+				&& pip install --upgrade ansible-core passlib \
+        && ansible-galaxy collection install --upgrade \
+            community.general community.crypto \
+				&& ansible-playbook $scriptdir/ssl-certificates-setup.yml --ask-become-pass
+
 	echo "Setup completed for all packages."
 }
 
