45eccc644c8a6f933ea9f185aa91d4c2b06a4839
Abdel Benamara <abdelb.contact@gmail.com>
2025-08-18 15:10:29 +0200

    feat(tournaments): add support for tournaments creation, edition and deletion on the client side while persisting data in local storage

--- packages/frontend/src/client/index.ts
+++ packages/frontend/src/client/index.ts
@@ -6,39 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 14:27:16 by abenamar          #+#    #+#             */
-/*   Updated: 2025/08/18 02:58:11 by abenamar         ###   ########.fr       */
+/*   Updated: 2025/08/18 15:04:51 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import { loadPong } from "#pong";
+import { loadTournaments } from "#tournaments";
 import htmx from "htmx.org";
-
-function loadTournaments() {
-  const upcoming = [
-    { name: "Summer Showdown", date: "2025-07-15" },
-    { name: "July Clash", date: "2025-07-30" },
-  ];
-  const terminated = [{ name: "Spring Cup", date: "2025-05-20" }];
-  const up = document.getElementById("upcomingList")!;
-
-  up.replaceChildren();
-  upcoming.forEach((t) => {
-    const li = document.createElement("li");
-    li.textContent = `${t.name} - ${t.date}`;
-
-    up.appendChild(li);
-  });
-
-  const term = document.getElementById("terminatedList")!;
-
-  term.replaceChildren();
-  terminated.forEach((t) => {
-    const li = document.createElement("li");
-    li.textContent = `${t.name} - ${t.date}`;
-
-    term.appendChild(li);
-  });
-}
 
 function hydrate() {
   const routes = {
